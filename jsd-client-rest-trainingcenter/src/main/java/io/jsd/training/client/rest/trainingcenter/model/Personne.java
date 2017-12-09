8da33d6788a8f7429be02ed24067f380da6068a7
Jean-Sébastien Dumas <jsdumas@free.fr>
2017-12-10 00:30:54 +0100

    add client rest training center project

---
+++ jsd-client-rest-trainingcenter/src/main/java/io/jsd/training/client/rest/trainingcenter/model/Personne.java
@@ -0,0 +1,55 @@
+package io.jsd.training.client.rest.trainingcenter.model;
+
+
+public abstract class Personne {
+	
+	protected String nom;
+	protected String prenom;
+
+	
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((nom == null) ? 0 : nom.hashCode());
+		result = prime * result + ((prenom == null) ? 0 : prenom.hashCode());
+		return result;
+	}
+
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Personne other = (Personne) obj;
+		if (nom == null) {
+			if (other.nom != null)
+				return false;
+		} else if (!nom.equals(other.nom))
+			return false;
+		if (prenom == null) {
+			if (other.prenom != null)
+				return false;
+		} else if (!prenom.equals(other.prenom))
+			return false;
+		return true;
+	}
+	
+	
+	
+	
+	public String getNom() {
+		return nom;
+	}
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+	public String getPrenom() {
+		return prenom;
+	}
+	public void setPrenom(String prenom) {
+		this.prenom = prenom;
+	}
+	
+}
