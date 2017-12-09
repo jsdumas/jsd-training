fdc823064c7349fbf687ab98b49f66fa43007ac6
Jean-Sébastien Dumas <jsdumas@free.fr>
2017-12-09 17:59:35 +0100

    add project spring data

---
+++ jsd-spring-data/src/main/java/io/jsd/training/springdata/dao/ClientDAO.java
@@ -0,0 +1,26 @@
+package io.jsd.training.springdata.dao;
+
+import java.util.List;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import io.jsd.training.springdata.model.Client;
+
+@Repository
+public interface ClientDAO extends CrudRepository<Client, Long>{
+	
+	Page<Client> findAll(Pageable page);
+	
+//	@Query("select c from Client c where c.nom = :nom")
+	List<Client> findByNom(String nom);
+	
+	@Query("select c from Client c where c.prenom = :prenom")
+	List<Client> findByPrenomClient(@Param("prenom") String prenom);
+
+}
---
+++ jsd-spring-data/src/main/java/io/jsd/training/springdata/service/ClientService.java
@@ -0,0 +1,34 @@
+package io.jsd.training.springdata.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import io.jsd.training.springdata.dao.ClientDAO;
+import io.jsd.training.springdata.model.Client;
+
+@Service
+public class ClientService {
+
+	@Autowired
+	private ClientDAO clientDAO;
+
+	@Transactional
+	public Client save(Client client) {
+		return clientDAO.save(client);
+	}
+
+	public List<Client> findByNom(String nom) {
+		return clientDAO.findByNom(nom);
+	}
+
+	public List<Client> findAll() {
+		Page<Client> resultPage = clientDAO.findAll(new PageRequest(0, 5));
+		return resultPage.getContent();
+	}
+
+}
