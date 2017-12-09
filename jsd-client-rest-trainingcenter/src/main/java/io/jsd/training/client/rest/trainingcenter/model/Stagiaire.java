b5ee749ed6230695b13c0a0607f657804b93d65e
Jean-Sébastien Dumas <jsdumas@free.fr>
2017-12-10 00:30:54 +0100

    add client rest training center project

---
+++ jsd-client-rest-trainingcenter/src/main/java/io/jsd/training/client/rest/trainingcenter/model/Stagiaire.java
@@ -0,0 +1,115 @@
+package io.jsd.training.client.rest.trainingcenter.model;
+
+import java.io.Serializable;
+import java.util.Date;
+import java.util.List;
+
+
+public class Stagiaire extends Personne implements Serializable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	private Integer id;
+	private Date dateDeNaissance;
+	private String email;
+	private Integer nbJourDispo;
+//	@OneToMany(mappedBy="stagiaire")
+//	private List<Evaluation> evaluations;
+
+	
+	public Stagiaire() {
+		// TODO Auto-generated constructor stub
+	}
+
+
+	public Stagiaire(Integer id, Date dateDeNaissance, String email,
+			Integer nbJourDispo) {
+		super();
+		this.id = id;
+		this.dateDeNaissance = dateDeNaissance;
+		this.email = email;
+		this.nbJourDispo = nbJourDispo;
+	}
+
+
+	public String toString() {
+		return "Stagiaire [id=" + id + ", dateDeNaissance=" + dateDeNaissance
+				+ ", email=" + email + ", nbJourDispo=" + nbJourDispo + "]";
+	}
+
+
+	public int hashCode() {
+		final int prime = 31;
+		int result = super.hashCode();
+		result = prime * result
+				+ ((dateDeNaissance == null) ? 0 : dateDeNaissance.hashCode());
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+
+
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (!super.equals(obj))
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Stagiaire other = (Stagiaire) obj;
+		if (dateDeNaissance == null) {
+			if (other.dateDeNaissance != null)
+				return false;
+		} else if (!dateDeNaissance.equals(other.dateDeNaissance))
+			return false;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
+
+
+	public Integer getId() {
+		return id;
+	}
+
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+
+	public Date getDateDeNaissance() {
+		return dateDeNaissance;
+	}
+
+
+	public void setDateDeNaissance(Date dateDeNaissance) {
+		this.dateDeNaissance = dateDeNaissance;
+	}
+
+
+	public String getEmail() {
+		return email;
+	}
+
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+
+	public Integer getNbJourDispo() {
+		return nbJourDispo;
+	}
+
+
+	public void setNbJourDispo(Integer nbJourDispo) {
+		this.nbJourDispo = nbJourDispo;
+	}
+
+}
---
+++ jsd-client-rest-trainingcenter/src/main/java/io/jsd/training/client/rest/trainingcenter/runtime/Runtime.java
@@ -0,0 +1,156 @@
+package io.jsd.training.client.rest.trainingcenter.runtime;
+
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.apache.commons.io.LineIterator;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpDelete;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.client.methods.HttpPut;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+
+import com.fasterxml.jackson.core.JsonParseException;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import io.jsd.training.client.rest.trainingcenter.model.Stagiaire;
+
+
+public class Runtime {
+
+	public static void main(String[] args) throws JsonParseException, JsonMappingException, ClientProtocolException, IOException {
+		ObjectMapper mapper = new ObjectMapper();
+		
+		Stagiaire stagiaireGet = mapper.readValue(sendGetRequest(), Stagiaire.class);
+		Stagiaire stagiairePost = mapper.readValue(sendPostRequest(), Stagiaire.class);
+//		Stagiaire stagiaireDelete = mapper.readValue(sendDeleteRequest(), Stagiaire.class);
+		sendDeleteRequest();
+		Stagiaire stagiaireUpdate = mapper.readValue(sendUpdateRequest(), Stagiaire.class);
+		
+		System.out.println(stagiaireGet);
+		System.out.println(stagiairePost);
+		System.out.println(stagiaireUpdate);
+	}
+	
+	
+	public static String sendGetRequest() throws ClientProtocolException, IOException {
+
+		CloseableHttpClient closeableHttpClient = HttpClients.createDefault();
+		HttpGet httpGet = new HttpGet(
+				"http://localhost:8080/CentreFormation/rest/stagiaire/id/3");
+		
+
+		CloseableHttpResponse response = closeableHttpClient.execute(httpGet);
+
+		StringBuilder builder = new StringBuilder();
+//		BufferedReader bufferedReader = new BufferedReader(
+//				new InputStreamReader(response.getEntity().getContent()));
+		
+		
+		LineIterator iterator = new LineIterator(new InputStreamReader(response
+				.getEntity().getContent()));
+
+		while (iterator.hasNext()) {
+			builder.append(iterator.nextLine());
+		}
+		
+		return builder.toString();
+	}
+	
+	
+	public static String sendPostRequest() throws ClientProtocolException, IOException {
+
+		CloseableHttpClient closeableHttpClient = HttpClients.createDefault();
+		
+		HttpPost httpPost = new HttpPost(
+				"http://localhost:8080/CentreFormation/rest/stagiaire");
+		
+		httpPost.setHeader("Accept", "application/json");
+		httpPost.setHeader("Content-type", "application/json");
+		
+		Map<String, Object> params = new HashMap<String, Object>();
+		params.put("nom", "POST");
+		params.put("prenom", "DO");
+		
+		ObjectMapper mapper = new ObjectMapper();
+		
+		StringEntity stringEntity = new StringEntity(
+				mapper.writeValueAsString(params));
+		
+		httpPost.setEntity (stringEntity);
+		
+		CloseableHttpResponse response = closeableHttpClient.execute(httpPost);
+
+		StringBuilder builder = new StringBuilder();
+//		BufferedReader bufferedReader = new BufferedReader(
+//				new InputStreamReader(response.getEntity().getContent()));
+		
+		
+		LineIterator iterator = new LineIterator(new InputStreamReader(response
+				.getEntity().getContent()));
+
+		while (iterator.hasNext()) {
+			builder.append(iterator.nextLine());
+		}
+		
+		return builder.toString();
+	}
+	
+	
+	public static void sendDeleteRequest() throws ClientProtocolException, IOException {
+
+		CloseableHttpClient closeableHttpClient = HttpClients.createDefault();
+		
+		HttpDelete httpDelete = new HttpDelete(
+				"http://localhost:8080/CentreFormation/rest/stagiaire/id/5");
+				
+		CloseableHttpResponse response = closeableHttpClient.execute(httpDelete);
+		
+	}
+	
+	public static String sendUpdateRequest() throws ClientProtocolException, IOException {
+
+		CloseableHttpClient closeableHttpClient = HttpClients.createDefault();
+		
+		HttpPut httpPut = new HttpPut(
+				"http://localhost:8080/CentreFormation/rest/stagiaire");
+		
+		httpPut.setHeader("Accept", "application/json");
+		httpPut.setHeader("Content-type", "application/json");
+		
+		Map<String, Object> params = new HashMap<String, Object>();
+		params.put("id", 3);
+		params.put("nom", "LUCIDA");
+		params.put("prenom", "MARION");
+		
+		ObjectMapper mapper = new ObjectMapper();
+		
+		StringEntity stringEntity = new StringEntity(
+				mapper.writeValueAsString(params));
+		
+		httpPut.setEntity (stringEntity);
+				
+		CloseableHttpResponse response = closeableHttpClient.execute(httpPut);
+		
+		StringBuilder builder = new StringBuilder();
+
+		
+		LineIterator iterator = new LineIterator(new InputStreamReader(response
+				.getEntity().getContent()));
+
+		while (iterator.hasNext()) {
+			builder.append(iterator.nextLine());
+		}
+		
+		return builder.toString();
+		
+	}
+
+}
