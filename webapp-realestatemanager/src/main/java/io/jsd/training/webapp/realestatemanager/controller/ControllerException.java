c292a0b6cd750d508fda2ce2866b88abd4899eb3
Jean-Sébastien Dumas <jsdumas@softwaymedical.fr>
2017-12-14 13:50:32 +0100

    real estate refactor

--- webapp-realestatemanager/src/main/java/io/jsd/training/webapp/realestatemanager/controller/ControllerException.java
+++ webapp-realestatemanager/src/main/java/io/jsd/training/webapp/realestatemanager/controller/ControllerException.java
@@ -2,9 +2,6 @@ package io.jsd.training.webapp.realestatemanager.controller;
 
 public class ControllerException extends Exception {
 	
-	/**
-	 * 
-	 */
 	private static final long serialVersionUID = 1L;
 	
 	public ControllerException(String message, Throwable e){
--- webapp-realestatemanager/src/main/java/io/jsd/training/webapp/realestatemanager/service/ServiceException.java
+++ webapp-realestatemanager/src/main/java/io/jsd/training/webapp/realestatemanager/service/ServiceException.java
@@ -2,9 +2,6 @@ package io.jsd.training.webapp.realestatemanager.service;
 
 public class ServiceException extends Exception {
 
-	/**
-	 * 
-	 */
 	private static final long serialVersionUID = 1L;
 	
 	public ServiceException(String message, Throwable e){
