25a556f9678740cb8e29f859505d9de55f388434
Jean-Sébastien Dumas <jsdumas@free.fr>
2017-12-16 23:31:50 +0100

    Java design pattern project to java best of Guava collection tuto

---
+++ java-best-of/src/main/java/io/jsd/training/java/guava/README.md
@@ -0,0 +1,49 @@
+## GOOGLE GUAVA
+
+Packages :
+-annotations
+-base
+-collect
+-io
+-net
+-primitives
+-concurrent
+
+#Annotations
+4 annotations
+GWT compatibility
+@Beta : classes susceptibles de changer
+@VisibleForTesting : méthode visible uniquement pour les tests
+
+#Base
+characters and Strings manipulation
+-CaseFormat
+-CharMatcher = readable regex
+-Charsets
+-Joiner
+-Splitter
+-Strings
+
+Fonctionnal programming
+-Function(s)
+-Predicate (s)
+-Supplier (s)
+
+-Closures
+-Lambdas
+-Scalas
+
+Preconditions : business code more readable
+
+#Others
+-Defaults
+-FinalizableReference
+-Objects
+-Preconditions
+-Throwables
+
+
+#Exemples
+https://www.leveluplunch.com/java/tutorials/008-using-guava-fluent-ordering-comparator-part1/
+https://www.leveluplunch.com/java/tutorials/012-guava-ordering-part2/
+
---
+++ java-best-of/src/main/java/io/jsd/training/java/guava/collection/ListsTranform.java
@@ -0,0 +1,34 @@
+package io.jsd.training.java.guava.collection;
+
+import java.util.List;
+
+import com.google.common.base.Function;
+import com.google.common.base.Splitter;
+import com.google.common.collect.Lists;
+
+public class ListsTranform {
+	
+	public static void main(String[] args) {
+	// Given words list
+	List<String> words = Splitter.onPattern(" ").splitToList( "Lorem ipsum dolor sit amet consectetur adipisicing elit");
+	 
+	//Go through a list of objects
+	// Lists.transform returns a list that applies function to each
+	// element of collection. The returned list is a transformed view
+	// of input list.
+	List<String> transformedWords = Lists.transform(
+	        words,
+	        //Anonymous class
+	        new Function<String, String>() {
+	            @Override
+	            public String apply(String input) {
+	                return input.toUpperCase().substring(0, 3);
+	            }
+	        }
+	);
+	 
+	System.out.println(transformedWords);
+	        // -> [LOR, IPS, DOL, SIT, AME, CON, ADI, ELI]
+	}
+
+}
