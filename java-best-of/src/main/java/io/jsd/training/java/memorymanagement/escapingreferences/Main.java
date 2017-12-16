5e27b376d19ce2d146bea5d08c53dc7b3eccde05
Jean-Sébastien Dumas <jsdumas@free.fr>
2017-12-17 00:43:54 +0100

    escaping reference book catalog

--- java-best-of/src/main/java/io/jsd/training/java/memorymanagement/escapingreferences/Main.java
+++ java-best-of/src/main/java/io/jsd/training/java/memorymanagement/escapingreferences/Main.java
@@ -6,11 +6,17 @@ public class Main {
 
 		records.addCustomer(new Customer("John"));
 		records.addCustomer(new Customer("Simon"));
+		
+		//this method fails because it tries to clear an immutable collection
+//		records.getCustomers().clear();
 				
 		for (Customer next : records.getCustomers().values())
 				{
 					System.out.println(next);
 				}
+		
+		Customer john = records.getCustomerByName("John");
+		System.out.println(john.getName());
 
 	}
 	
---
+++ java-best-of/src/main/java/io/jsd/training/java/memorymanagement/escapingreferences/bookcatalog/Main.java
@@ -0,0 +1,19 @@
+package io.jsd.training.java.memorymanagement.escapingreferences.bookcatalog;
+
+
+import java.util.List;
+import java.util.Map;
+import java.util.function.Predicate;
+
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		BookCollection bc = new BookCollection();
+		
+		//get price of book called Tom Jones in EUR
+		System.out.println(bc.findBookByName("Tom Jones").getPrice().convert("EUR"));
+		
+	}
+}
