c8c181bad16aeb5cead4f61038efd173b9a64dc9
Jean-Sébastien Dumas <jsdumas@softwaymedical.fr>
2017-12-18 13:09:28 +0100

    Garbage collector and finalize method TP

---
+++ java-best-of/src/main/java/io/jsd/training/java/memorymanagement/garbagecollector/Customer.java
@@ -0,0 +1,38 @@
+package io.jsd.training.java.memorymanagement.garbagecollector;
+
+public class Customer  {
+	private String name;
+
+	public String getName() {
+		return name;
+	}
+	
+	public Customer(String name) {
+		this.name = name;
+	}
+	
+	public Customer(Customer oldCustomer) {
+		this.name = oldCustomer.name; 
+	}
+	
+	
+	public String toString() {
+		return name;
+	}
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	//it is useless
+	//if GC never run for this object, finalize method will never called
+	public void finalalize() {
+		System.out.println("This object is being gc'd.");
+		
+		//But it is very usefull if we want to verify or log information before object erasing
+//		if(file.isOpen()) {
+//			logger.warn("It looks like this resource was not closed");
+//		}
+	}
+	
+}
