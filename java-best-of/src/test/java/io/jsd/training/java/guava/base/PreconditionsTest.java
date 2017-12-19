d0ed2b971bfcc27c8253d9c65be0057c66f13d64
Jean-Sébastien Dumas <jsdumas@free.fr>
2017-12-19 12:56:49 +0100

    Guava preconditions test

---
+++ java-best-of/src/test/java/io/jsd/training/java/guava/base/PreconditionsTest.java
@@ -0,0 +1,47 @@
+package io.jsd.training.java.guava.base;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
+
+import org.hamcrest.MatcherAssert;
+import org.hamcrest.Matchers;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
+
+import com.google.common.base.Preconditions;
+
+public class PreconditionsTest {
+	
+	@Rule
+	public ExpectedException expectedException = ExpectedException.none();
+	
+	private void methodWithNotNullParam(String param) {
+		Preconditions.checkNotNull(param, "Parameter '%s' must not be null", "param");
+	}
+	 
+	private void methodWithValidParam(String param) {
+		Preconditions.checkArgument(param.trim().length() > 0, "Parameter '%s' must not be empty", "param");
+	}
+	 
+	@Test
+	public void testMethodWithNotNullParam() {
+		try {
+			methodWithNotNullParam(null);
+			expectedException.expect(NullPointerException.class);
+		} catch (NullPointerException npe) {
+			assertThat("Parameter 'param' must not be null", equalTo(npe.getMessage()));
+		}
+	}
+	 
+	@Test
+	public void testMethodWithValidParam() {
+		try {
+			methodWithValidParam("");
+			expectedException.expect(IllegalArgumentException.class);
+		} catch (IllegalArgumentException iae) {
+			assertThat("Parameter 'param' must not be empty", equalTo(iae.getMessage()));
+		}
+	}
+
+}
