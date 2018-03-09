7fbadef6b3758c5997a0b49d99649097b06a51ff
Jean-Sébastien Dumas <jsdumas@free.fr>
2018-03-09 14:45:43 +0100

    Design Pattern Chain of responsibility

---
+++ java-best-of/src/main/java/io/jsd/training/designpattern/behavioural/chainofresponsibility/FileLogger.java
@@ -0,0 +1,14 @@
+package io.jsd.training.designpattern.behavioural.chainofresponsibility;
+
+public class FileLogger extends AbstractLogger {
+
+	public FileLogger(int level) {
+		this.level = level;
+	}
+
+	@Override
+	protected void write(String message) {
+		System.out.println("File::Logger: " + message);
+	}
+
+}
