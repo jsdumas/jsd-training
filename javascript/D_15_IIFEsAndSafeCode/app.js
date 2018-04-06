93941e25a2ef8c83cbd4c8679278c08e157c50c1
Jean-Sébastien Dumas <jsdumas@free.fr>
2018-04-06 17:03:35 +0200

    Javascript injection of window object in IIFE

--- javascript/D_15_IIFEsAndSafeCode/app.js
+++ javascript/D_15_IIFEsAndSafeCode/app.js
@@ -2,6 +2,8 @@
 (function(global, name) {
     
     var greeting = 'Hello';
+    //global meens we change vamue of greeting in window object
+    //if we don't do that, console.log print value of first declaration of greeting in global context (window.greeting = 'Hola') 
     global.greeting = 'Hello';
     console.log(greeting + ' ' + name);
     
