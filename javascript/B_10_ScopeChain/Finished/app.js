0239d48d49b24a5a5df6c267c7c18098da1bf256
Jean-Sébastien Dumas <jsdumas@free.fr>
2018-04-06 16:18:29 +0200

    Javascript Scope Chain

--- javascript/B_10_ScopeChain/Finished/app.js
+++ javascript/B_10_ScopeChain/Finished/app.js
@@ -1,11 +1,20 @@
 function a() {
-    
-    function b() {
-        console.log(myVar);
-    }
-    
+	function b() {
+		//Call myVar in Global context
+		console.log(myVar);
+	}
+	b();
+}
+
+function aa() {
+	var myVar = 2;
+	function b() {
+		//call myVar in aa context
+		console.log(myVar);
+	}
 	b();
 }
 
 var myVar = 1;
 a();
+aa();
