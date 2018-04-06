c7ef5d85b1f9c60cbcf274e85165813e85456e7f
Jean-Sébastien Dumas <jsdumas@free.fr>
2018-04-06 17:16:53 +0200

    Javascript closure 2 - D16b

--- javascript/D_16b_ClosuresPart2/app.js
+++ javascript/D_16b_ClosuresPart2/app.js
@@ -15,8 +15,10 @@ function buildFunctions() {
     return arr;
 }
 
+//First invocation of this function set an array and memorise in closure variable i with 3 as a value
 var fs = buildFunctions();
 
+//so that every time we invoke function of an item of the arrray, we get 3 as result in console.log
 fs[0]();
 fs[1]();
 fs[2]();
@@ -27,6 +29,7 @@ function buildFunctions2() {
     
     for (var i = 0; i < 3; i++) {
         arr.push(
+        		//here IIFE stock in each item of the array the value that tale var i for each iteration
             (function(j) {
                 return function() {
                     console.log(j);   
