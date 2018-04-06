4ce990f14d28ac79dd3a9bef2d216dd220c7a020
Jean-Sébastien Dumas <jsdumas@free.fr>
2018-04-06 16:23:52 +0200

    Javascript JSON

--- javascript/D__4_JSON/app.js
+++ javascript/D__4_JSON/app.js
@@ -3,8 +3,10 @@ var objectLiteral = {
     isAProgrammer: true
 }
 
+//JSON to String
 console.log(JSON.stringify(objectLiteral));
 
+//String to JSON
 var jsonValue = JSON.parse('{ "firstname": "Mary", "isAProgrammer": true }');
 
 console.log(jsonValue);
