1a6b7e9f27d15d4af12712a484b7d102e7f8b604
Jean-Sébastien Dumas <jsdumas@free.fr>
2018-04-09 14:07:51 +0200

    Javascript - J3

--- javascript/J__3_OurObjectAndPrototype/Greetr.js
+++ javascript/J__3_OurObjectAndPrototype/Greetr.js
@@ -1,14 +1,17 @@
 (function(global, $) {
     
+	//this function return a object, this is not a function constructor
     var Greetr = function(firstName, lastName, language) {
         return new Greetr.init(firstName, lastName, language);   
     }
     
     Greetr.prototype = {};
     
+    //this is a function constructor
     Greetr.init = function(firstName, lastName, language) {
-        
+    	// self memorise this        
         var self = this;
+        // init property with default value if needed
         self.firstName = firstName || '';
         self.lastName = lastName || '';
         self.language = language || 'en';
