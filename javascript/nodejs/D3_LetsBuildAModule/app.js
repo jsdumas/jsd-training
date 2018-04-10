ec6cea4757c0eb7ae4b659acd9ae03b5167bd694
Jean-Sébastien Dumas <jsdumas@free.fr>
2018-04-10 14:51:35 +0200

    Node js --> build a module

---
+++ javascript/nodejs/D3_LetsBuildAModule/app.js
@@ -0,0 +1,4 @@
+//require show the path where the js file is located
+// and get the code of file greet.js
+var greet = require('./greet');
+greet();
---
+++ javascript/nodejs/D3_LetsBuildAModule/greet.js
@@ -0,0 +1,6 @@
+var greet = function() {
+	console.log('Hello!');
+};
+
+//allow to export previous code
+module.exports = greet;
